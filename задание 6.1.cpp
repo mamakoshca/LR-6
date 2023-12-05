839f87a903e1ef14fceef5bde3b0da4a02208ce2
mamakoshca <enotartur5@gmail.com>
2023-12-05 16:25:38 +0700

    LR-6
    LR-6

---
+++ задание 6.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Дана матрица размера M × N. Для каждого столбца матрицы найти
+произведение его элементов.
+*/
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int m, n, p;
+    cout << "Введите значение М: ";
+    cin >> m;
+    cout << "Введите значение N: ";
+    cin >> n;
+    int** a = new int* [m];
+    for (int i = 0; i < m; i++) {
+        a[i] = new int[n];
+    }
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "Введите элемент матрицы под номером " << i + 1 << "." << j + 1 << ": ";
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        p = 1;
+        for (int j = 0; j < m; ++j)
+            p *= a[j][i];
+        cout << "Произведение первого столбца = " << p << endl;
+    }
+
+    for (int i = 0; i < m; i++) {
+        delete[]a[i];
+    }
+    delete []a;
+}
---
+++ задание 6.2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+/*
+Дана квадратная матрица A порядка M. Найти минимальный элемент для каждой ее диагонали,
+параллельной главной (начиная с одноэлементной диагонали A1,M ).
+*/
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int m,v;
+	cout << "Введите ранг матрицы: ";
+	cin >> m;
+
+	int** a = new int* [m];
+	for (int i = 0; i < m; i++) {
+		a[i] = new int[m];
+	}
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << "Элемент " << i + 1 << "." << j + 1 << " ";
+			cin >> a[i][j];
+		}
+	}
+
+	//Нижняя часть
+	for (int i = 0; i < m; i++) {
+		int min = a[m - 1 - i][0];
+		for (int j = i; j >= 0; j--) {
+			v = a[m - 1 - j][i - j];
+			if (min > v) {
+				min = v;
+			}
+		}
+		cout << "min = " << min << endl;
+	}
+
+	//Верхняя часть
+	for (int j = 1; j < m; j++) {
+		int min = a[0][j];
+		for (int i = j; i < m; i++) {
+			v = a[i - j][i];
+			if (min > v) {
+				min = v;
+			}
+		}
+		cout << "min = " << min << endl;
+	}
+	for (int i = 0; i < m; i++) {
+		delete[] a[i];
+	}
+	delete[] a;
+}
+
